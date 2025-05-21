7ccbc69bdde046914821f7818c862f1867632493
OblizatoR01 <zalizator@gmail.com>
2025-05-21 12:29:03 +0300

    Добавлены начальные данные

--- laba/main.cpp
+++ laba/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #7.Banking operarions \n";
+    cout << "Author: Yuri Lovchik\n";
     return 0;
 }
