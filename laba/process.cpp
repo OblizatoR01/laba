47be7462999ea6865376e1f4fa2b444b648090f9
OblizatoR01 <zalizator@gmail.com>
2025-05-28 19:54:17 +0300

    Add processing.h and process.cpp with stub implementation

---
+++ laba/process.cpp
@@ -0,0 +1,6 @@
+// process.cpp
+#include "processing.h"
+
+int process() {
+    return 0; // «аглушка, возвращающа€ значение по умолчанию
+}
---
+++ laba/processing.h
@@ -0,0 +1,7 @@
+// processing.h
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+int process();
+
+#endif // PROCESSING_H
